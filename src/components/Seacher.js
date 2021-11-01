7740dd34053ed34a6c38c060fdd6717a4f9059f7
Zairo Bastos <zairo_vianahd@hotmail.com>
2021-11-01 12:14:32 -0300

    Finalizada a parte do formik

--- src/components/Seacher.js
+++ src/components/Seacher.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import '../styles/seacher.scss'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSeacher = Yup.object().shape({
+    seacher: Yup.string()
+    .min(3,"Minímo 3 caracteres")
+    .required("Campo Obrigatório")
+})
 
 export default function Seacher() {
+    const formik = useFormik({
+        validationSchema: validationSeacher,
+        validateOnChange: false,
+        initialValues:{
+            seacher:'',
+        },
+        onSubmit: (value) =>{
+            console.log(value);
+        }
+    })
     return (
-    <>
+    <form onSubmit={formik.handleSubmit}>
         <div className="pesquisar">
             <input
                 type="text"
+                name="seacher"
+                id="seacher"
                 className=""
                 placeholder="Busque por uma cidade..."
+                onChange={formik.handleChange}
+                values={formik.values.seacher}
             />
             <button type="submit">Pesquisar</button>
+            {formik.errors.seacher ? <div className="error">{formik.errors.seacher}</div>: null}
         </div>
-    </>
+    </form>
     );
 }
